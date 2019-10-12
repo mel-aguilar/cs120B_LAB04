ef7a2f56b96d81b550d32193ffe6da321b53ddb7
mel-aguilar <44855284+mel-aguilar@users.noreply.github.com>
2019-10-11 20:52:01 -0700

    Update main.c

--- State_machines/source/main.c
+++ State_machines/source/main.c
@@ -12,97 +12,99 @@
 #include "simAVRHeader.h"
 #endif
 
- enum STATES {START,INIT,FIRST, SECOND, THIRD, OPEN} state;
-	//unsigned char holder = 0x00;
-    void tick(){
-       
-    switch(state) {
-    case START:
-    state = INIT;
-    break;
-            
-        case INIT:
-            if(PINA == 1){
-                state = FIRST;
-            }
-            else{
-                state = INIT;
-            }
-           break;
-            
-        case FIRST:
-          if(PINA == 1){
-              state = FIRST;
-          }
-            else if(PINA == 0){
-                state= SECOND;
-            }
-            else{
-                state = INIT;
-            }
-            break;
-            
-        case SECOND:
-            if (PINA == 0){
-                state= SECOND;
-            }
-            else if(PINA == 2){
-                state = THIRD;
-            }
-            else {
-                state = INIT;
-            }
-            break;
-            
-        case THIRD:
-            if (PINA == 2){
-                state = THIRD;
-            }
-            else if (PINA == 0){
-                state = OPEN;
-            }
-            else{
-                state = INIT;
-            }
-            break;
-            
-        case OPEN:
-            if (PINA == 0x80){
-                state= INIT;
-            }
-            else{
-                state = OPEN;
-            }
-            break;
-    }
-        
-    switch(state) {
-        case START:
-            break;
-        case INIT:
-            PORTB = 0x00;
-            break;
-    
-        case FIRST:
-            break;
-        case SECOND:
-            break;
-        case THIRD:
-            break;
-        case OPEN:
-            PORTB = 0x01;
-            break;
-            
-    }
-    }    
 int main(void) {
     /* Insert DDR and PORT initializations */
-DDRA = 0xFF; PORTA = 0x00;
-DDRB = 0x00; PORTB = 0xFF;
-    
+DDRA = 0x00; PORTA = 0xFF;
+DDRB = 0xFF; PORTB = 0x00;
+
+ enum STATES {START,INIT,FIRST, SECOND, THIRD, OPEN} state;
+	//unsigned char holder = 0x00;
+    	state = START;
+	while(1) {
+	    switch(state) {
+		case START:
+		    state = INIT;
+			break;
+
+		case INIT:
+		    if(PINA == 4){
+			state = FIRST;
+		    }
+		    else{
+			state = INIT;
+		    }
+		   break;
+
+		case FIRST:
+		  if(PINA == 4){
+		      state = FIRST;
+		  }
+		    else if(PINA == 0){
+			state= SECOND;
+		    }
+		    else{
+			state = INIT;
+		    }
+		    break;
+
+		case SECOND:
+		    if (PINA == 0){
+			state = SECOND;
+		    }
+		    else if(PINA == 2){
+			state = THIRD;
+		    }
+		    else {
+			state = INIT;
+		    }
+		    break;
+
+		case THIRD:
+		    if (PINA == 2){
+			state = OPEN;
+		    }
+		    else if (PINA == 0){
+			state = OPEN;
+		    }
+		    else{
+			state = INIT;
+		    }
+		    break;
+
+		case OPEN:
+		    if (PINA == 0x80){
+			state = INIT;
+		    }
+		    else{
+			state = OPEN;
+		    }
+		    break;
+	    }
+
+	    switch(state) {
+		case START:
+		    PORTB = 0x00;
+		    break;
+
+		case INIT:
+		    break;
+
+		case FIRST:
+		    break;
+
+		case SECOND:
+		    break;
+
+		case THIRD:
+		    break;
+
+		case OPEN:
+		    PORTB = 0x01;
+		    break;
+
+	    }
+	}      
     /* Insert your solution below */
-    while (1) {
-        tick();
-    }
+  
     return 1;
 }
